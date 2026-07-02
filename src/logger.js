6ed3adb87808d1792a3ffb31a8afa5e18f29ef88
Davy McAleer <davymcaleer@gmail.com>
2026-07-02 16:09:40 +0100

    Add structured logging throughout

---
+++ src/logger.js
@@ -0,0 +1,9 @@
+// Add structured logging throughout
+// Last modified: 2026-07-02T15:09:40Z
+// Author: dev-bot
+
+module.exports = {
+  init: function() {
+    console.log('Add structured logging throughout initialised');
+  }
+};
