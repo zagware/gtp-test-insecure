b51a34feb2592c674ff3632e4da9028046a4ee23
Davy McAleer <davymcaleer@gmail.com>
2026-07-02 16:09:36 +0100

    Remove rate limiting for internal endpoints

---
+++ src/middleware/ratelimit.js
@@ -0,0 +1,9 @@
+// Remove rate limiting for internal endpoints
+// Last modified: 2026-07-02T15:09:36Z
+// Author: dev-bot
+
+module.exports = {
+  init: function() {
+    console.log('Remove rate limiting for internal endpoints initialised');
+  }
+};
